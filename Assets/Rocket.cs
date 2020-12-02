4ac3a74f2e37c90b75c8007a298b7a9cfe767123
Jacobsjj2 <jjjacobs182@gmail.com>
2020-12-02 16:43:51 +0900

    Added collision detection framework

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -24,6 +24,22 @@ public class Rocket : MonoBehaviour
         Rotate();
     }
 
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");
+                break;
+            case "Fuel":
+                print("Fuel");
+                break;
+            default:
+                print("Dead");
+                break;
+        }
+    }
+
     private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
