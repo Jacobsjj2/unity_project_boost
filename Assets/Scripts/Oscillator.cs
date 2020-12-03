c7e0b42a208225c2d3f020e391b9a21bdb65c153
Jacobsjj2 <jjjacobs182@gmail.com>
2020-12-03 18:18:40 +0900

    Added level 3 and set up oscillating obstacle

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector;
+    Vector3 startVector;
+
+    [Range(0,1)]
+    [SerializeField]
+    float movementFactor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startVector = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 offsetVector = movementFactor * movementVector;
+        transform.position = startVector + offsetVector;
+    }
+}
