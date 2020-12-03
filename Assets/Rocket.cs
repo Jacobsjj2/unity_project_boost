c809de560b741832ff834da882c9f7f9af555a76
Jacobsjj2 <jjjacobs182@gmail.com>
2020-12-03 15:04:30 +0900

    Added delay on death and success

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,41 +6,74 @@ using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
-    Rigidbody rigidBody;
-    AudioSource audioSource;
+    private Rigidbody rigidBody;
+    private AudioSource audioSource;
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 100f;
+
+    private const float timeBetweenLevels = 1f;
+
+    enum State
+    {
+        Alive,
+        Dying,
+        Transcending
+    }
+
+    State currentState;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
+        currentState = State.Alive;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (currentState != State.Alive)
+        {
+            audioSource.Stop();
+            return;
+        }
+
         Thrust();
         Rotate();
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        if (currentState != State.Alive)
+        {
+            return;
+        }
+
         switch (collision.gameObject.tag)
         {
             case "Friendly":
                 // Do nothing
                 break;
             case "Finish":
-                print("Finished");
-                SceneManager.LoadScene(1);
+                currentState = State.Transcending;
+                Invoke("LoadNextLevel", timeBetweenLevels);
                 break;
             default:
-                print("Dead");
-                SceneManager.LoadScene(0);
+                currentState = State.Dying;
+                Invoke("RestartGame", timeBetweenLevels);
                 break;
         }
+    }
+
+    private void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
     }
 
     private void Thrust()
