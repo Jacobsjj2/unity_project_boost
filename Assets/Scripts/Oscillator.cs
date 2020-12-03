3eb34d5bfb8bf81d29f5976a6b93256d5a0cbc8f
Jacobsjj2 <jjjacobs182@gmail.com>
2020-12-03 18:39:17 +0900

    Implement oscillation

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -3,11 +3,11 @@
 [DisallowMultipleComponent]
 public class Oscillator : MonoBehaviour
 {
-    [SerializeField] Vector3 movementVector;
-    Vector3 startVector;
+    const float tau = 2f * Mathf.PI;
 
-    [Range(0,1)]
-    [SerializeField]
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] float period = 10f;
+    Vector3 startVector;
     float movementFactor;
 
     // Start is called before the first frame update
@@ -19,6 +19,13 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (period <= Mathf.Epsilon) { // Zero check that is float safe
+            return;
+        }
+
+        float cycles = Time.time / period;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+        movementFactor = (rawSinWave / 2f) + 0.5f;
         Vector3 offsetVector = movementFactor * movementVector;
         transform.position = startVector + offsetVector;
     }
